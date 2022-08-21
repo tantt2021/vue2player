917216649c24f9e519adea575455e838bb8ba0b5
ch-谭庭佳 <2746702220@qq.com>
2022-08-21 09:23:35 +0800

    process.vue

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -1,14 +1,14 @@
 <template>
   <div
     class="music-progress"
-    :style="{
-      width: control === 'volume' ? '10vw' : '40vw',
-    }"
     ref="progress"
     @click="barClick"
+    :style="{
+      width: control === 'volume' ? '150px' : '350px',
+    }"
   >
     <!-- 总长 -->
-    <div class="progress-total-bar"></div>
+    <!-- <div class="music-progress"></div> -->
     <!-- 缓冲 -->
     <div class="progress-outer" ref="percentProgress"></div>
     <!-- 已播放 -->
@@ -67,7 +67,7 @@ export default {
       }
     },
     percentProgress(newValue) {
-      let offsetWidth = this.$refs.progress.clientWidth * newValue;
+      let offsetWidth = (this.$refs.progress.clientWidth - 10) * newValue;
       this.$refs.percentProgress.style.width = `${offsetWidth}px`;
     },
   },
@@ -143,21 +143,35 @@ export default {
 <style lang="scss" scoped>
 .music-progress {
   margin: 16px 0;
-  width: 1000px;
+  padding: 5px;
+  // overflow: hidden;
+  width: 150px;
   // border: 1px solid #fff;
-  height: 2px;
-  background-color: #595755;
+  // height: 5px;
+  // background-color: #595755;
   margin-left: 10px;
   position: relative;
   cursor: pointer;
-
+  .progress-outer {
+    position: absolute;
+    top: 50%;
+    left: 5px;
+    display: inline-block;
+    width: 0;
+    height: 2px;
+    margin-top: -1px;
+    background: rgba(255, 255, 255, 0.2);
+  }
   .progress-inner {
     position: absolute;
     top: 50%;
-    left: 0px;
-    background-color: #fff;
+    // width: 0;
+    // left: 15px;
+    width: 150px;
     height: 2px;
-    // position: ;
+    background-color: #fff;
+    display: inline-block;
+    height: 2px;
     .dot {
       position: absolute;
       top: 50%;
